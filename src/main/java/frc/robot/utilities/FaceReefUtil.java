f70c5956f722564f6f552c483ab607552aa92923
SamueljWilson <115675537+SamueljWilson@users.noreply.github.com>
2025-02-02 14:37:15 -0800

    Fixed the indentation of FaceReefUtil and FaceStationUtil

--- src/main/java/frc/robot/utilities/FaceReefUtil.java
+++ src/main/java/frc/robot/utilities/FaceReefUtil.java
@@ -14,51 +14,51 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.Constants.FieldConstants;
 
 public class FaceReefUtil {
-    private final Translation2d m_reef;
+  private final Translation2d m_reef;
 
-    public FaceReefUtil() {
-        m_reef = getReef(getAlliance());
-    }
+  public FaceReefUtil() {
+    m_reef = getReef(getAlliance());
+  }
 
-    private static Alliance getAlliance() {
-        Optional<Alliance> allianceOpt = DriverStation.getAlliance();
-        Alliance alliance = allianceOpt.isPresent() ? allianceOpt.get() : Alliance.Blue;
-        return alliance;
-    }
+  private static Alliance getAlliance() {
+    Optional<Alliance> allianceOpt = DriverStation.getAlliance();
+    Alliance alliance = allianceOpt.isPresent() ? allianceOpt.get() : Alliance.Blue;
+    return alliance;
+  }
 
-    private static Translation2d getReef(Alliance alliance) {
-        Translation2d reef;
-        switch (alliance) {
-            default: assert false;
-            case Blue: {
-                reef = FieldConstants.kBlueReef;
-                break;
-            }
-            case Red: {
-                reef = FieldConstants.kRedReef;
-                break;
-            }
-        }
-        return reef;
+  private static Translation2d getReef(Alliance alliance) {
+    Translation2d reef;
+    switch (alliance) {
+      default: assert false;
+      case Blue: {
+        reef = FieldConstants.kBlueReef;
+        break;
+      }
+      case Red: {
+        reef = FieldConstants.kRedReef;
+        break;
+      }
     }
+    return reef;
+  }
 
-    private Translation2d getRobotToReef(Pose2d robotPose) {
-        Translation2d robotToReef = m_reef.minus(robotPose.getTranslation());
-        return robotToReef;
-    }
+  private Translation2d getRobotToReef(Pose2d robotPose) {
+    Translation2d robotToReef = m_reef.minus(robotPose.getTranslation());
+    return robotToReef;
+  }
 
-    // Calculates the desired angle from the robot's current angle
-    private Rotation2d getDesiredRotation(Pose2d robotPose) {
-        Rotation2d reefToRobotAngle = getRobotToReef(robotPose).getAngle();
-        Rotation2d desiredAngle = new Rotation2d(
-            Math.floor((reefToRobotAngle.getRadians() + (Math.PI/6.0)) / (Math.PI/3.0)) * (Math.PI/3.0) + Math.PI);
-        return desiredAngle;
-    }
+  // Calculates the desired angle from the robot's current angle
+  private Rotation2d getDesiredRotation(Pose2d robotPose) {
+    Rotation2d reefToRobotAngle = getRobotToReef(robotPose).getAngle();
+    Rotation2d desiredAngle = new Rotation2d(
+      Math.floor((reefToRobotAngle.getRadians() + (Math.PI/6.0)) / (Math.PI/3.0)) * (Math.PI/3.0) + Math.PI);
+    return desiredAngle;
+  }
 
-    public Rotation2d getRotationDeviation(Pose2d robotPose) {
-        Rotation2d currentRotation = robotPose.getRotation();
-        Rotation2d desiredRotation = getDesiredRotation(robotPose);
-        Rotation2d rotationDeviation = currentRotation.minus(desiredRotation);
-        return rotationDeviation;
-    }
+  public Rotation2d getRotationDeviation(Pose2d robotPose) {
+    Rotation2d currentRotation = robotPose.getRotation();
+    Rotation2d desiredRotation = getDesiredRotation(robotPose);
+    Rotation2d rotationDeviation = currentRotation.minus(desiredRotation);
+    return rotationDeviation;
+  }
 }
