e66680f7744e36244ba97dfeedbe098466de8721
keira1008 <>
2025-02-15 14:25:26 -0800

    create CoralPlacement

---
+++ src/main/java/frc/robot/commands/CoralPlacement.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.HandlerSubsystem;
+
+public class CoralPlacement extends SequentialCommandGroup {
+  private final DriveSubsystem m_drive;
+  private final HandlerSubsystem m_handler;
+
+  public CoralPlacement(DriveSubsystem drive, HandlerSubsystem handler) {
+    m_drive = drive;
+    m_handler = handler;
+    addRequirements(m_drive, m_handler);
+
+    Command driveToPose = new DriveToPose(null, drive); //TODO: get poses
+
+    addCommands(
+      driveToPose,
+      //TODO: strafe and align
+      //TODO: pivot and elevator (from crane subsystem)
+      Commands.runOnce(() -> m_handler.eject()),
+      Commands.waitUntil(() -> !m_handler.isLoadedCoral())
+    );
+  }
+}
