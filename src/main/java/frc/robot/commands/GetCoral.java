e74d466df62bbc51b280e0efd63bf00026986b05
keira1008 <>
2025-02-21 17:31:51 -0800

    change to parallel crane movement

--- src/main/java/frc/robot/commands/GetCoral.java
+++ src/main/java/frc/robot/commands/GetCoral.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -15,6 +16,8 @@ public class GetCoral extends SequentialCommandGroup {
   private final HandlerSubsystem m_handler;
   private final Crane m_crane;
   private final FieldPoseUtil m_fieldPoseUtil = new FieldPoseUtil();
+  private final Translation2d m_intakePos = new Translation2d(CraneConstants.kIntakeAngle, CraneConstants.kIntakeHeight);
+  private final Translation2d m_homePos = new Translation2d(CraneConstants.kPivotHome, CraneConstants.kElevatorHome);
 
   public GetCoral(DriveSubsystem drive, HandlerSubsystem handler, Crane crane, CoralStationSubPose subPose) {
     m_drive = drive;
@@ -26,15 +29,10 @@ public class GetCoral extends SequentialCommandGroup {
 
     addCommands(
       driveToPose,
-      Commands.runOnce(() -> m_crane.movePivotTo(CraneConstants.kIntakeAngle)),
-      Commands.waitUntil(() -> m_crane.atSetpoint().isPresent()),
-      Commands.runOnce(() -> m_crane.moveElevatorTo(CraneConstants.kIntakeHeight)),
-      Commands.waitUntil(() -> m_crane.atSetpoint().isPresent()),
+      Commands.runOnce(() -> m_crane.moveTo(m_intakePos)),
       Commands.runOnce(() -> m_handler.intakeCoral()),
       Commands.waitUntil(() -> m_handler.isLoadedCoral()),
-      Commands.runOnce(() -> m_crane.movePivotTo(CraneConstants.kPivotHome)),
-      Commands.waitUntil(() -> m_crane.atSetpoint().isPresent()),
-      Commands.runOnce(() -> m_crane.moveElevatorTo(CraneConstants.kElevatorHome)),
+      Commands.runOnce(() -> m_crane.moveTo(m_homePos)),
       Commands.waitUntil(() -> m_crane.atSetpoint().isPresent())
     );
   }
