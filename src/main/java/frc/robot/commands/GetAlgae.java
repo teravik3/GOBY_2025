27778f0740540d4357c4f387beeaac96f770bd28
keira1008 <>
2025-03-22 14:06:10 -0700

    refactoring GetAlgae

--- src/main/java/frc/robot/commands/GetAlgae.java
+++ src/main/java/frc/robot/commands/GetAlgae.java
@@ -2,7 +2,9 @@ package frc.robot.commands;
 
 import java.util.Set;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.CraneConstants;
@@ -17,10 +19,9 @@ import frc.robot.utilities.FieldPoseUtil.ReefSubPose;
 public class GetAlgae extends SequentialCommandGroup {
   private final DriveSubsystem m_drive;
   private final HandlerSubsystem m_handler;
-  private final FieldPoseUtil m_fieldPoseUtil;
   private final Crane m_crane;
+  private final FieldPoseUtil m_fieldPoseUtil;
   private AlgaeHeight m_algaeHeight;
-  private ReefPose m_reefHour;
 
   public GetAlgae(DriveSubsystem drive, HandlerSubsystem handler, Crane crane, 
       FieldPoseUtil fieldPoseUtil) {
@@ -29,18 +30,21 @@ public class GetAlgae extends SequentialCommandGroup {
     m_crane = crane;
     m_fieldPoseUtil = fieldPoseUtil;
     m_algaeHeight = null;
-    m_reefHour = null;
     addRequirements(m_drive, m_handler, m_crane);
 
     addCommands(
-      Commands.defer((() -> {return new DriveToPose(
-        m_fieldPoseUtil.getTargetPoseAtReef(m_fieldPoseUtil.closestReefHour(m_drive.getPose()), ReefSubPose.ALGAE),
-        drive);}), Set.of(drive)),
+      Commands.defer((() -> {
+        Pose2d pose = m_drive.getPose();
+        ReefPose reefHour = m_fieldPoseUtil.closestReefHour(pose);
+        m_algaeHeight = m_fieldPoseUtil.whichAlgaeHeight(reefHour);
+        Command driveToPose = new DriveToPose(
+          m_fieldPoseUtil.getTargetPoseAtReef(m_fieldPoseUtil.closestReefHour(pose), 
+          ReefSubPose.ALGAE),
+          drive);
+        return driveToPose;
+      }), Set.of(drive)),
 
-      Commands.defer((() -> {return Commands.runOnce(() -> m_reefHour = m_fieldPoseUtil.closestReefHour(m_drive.getPose()));}), Set.of()),
-      Commands.defer((() -> {return Commands.runOnce(() -> m_algaeHeight = m_fieldPoseUtil.whichAlgaeHeight(m_reefHour));}), Set.of()),
-
-      Commands.defer((() -> {return Commands.runOnce(() -> m_crane.moveTo(whichElevatorHeight(m_algaeHeight)));}), Set.of(crane)),
+      Commands.runOnce(() -> m_crane.moveTo(whichElevatorHeight(m_algaeHeight))),
       Commands.waitUntil(() -> m_crane.atGoal().isPresent()),
 
       Commands.runOnce(() -> m_handler.intakeAlgae()),
@@ -49,6 +53,8 @@ public class GetAlgae extends SequentialCommandGroup {
   }
  
   private Translation2d whichElevatorHeight(AlgaeHeight algaeHeight) {
-    return (algaeHeight == AlgaeHeight.DOWN) ? CraneConstants.kPositionLoAlgae : CraneConstants.kPositionHiAlgae;
+    return (algaeHeight == AlgaeHeight.DOWN)
+      ? CraneConstants.kPositionLoAlgae
+      : CraneConstants.kPositionHiAlgae;
   }
 }
