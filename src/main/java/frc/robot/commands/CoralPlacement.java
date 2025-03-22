9c27e9ab978c1edffe44b82fcaebe01adf0069c0
SamueljWilson <115675537+SamueljWilson@users.noreply.github.com>
2025-03-22 12:19:11 -0700

    Removed the strafe command and defered the driveToPose command in CoralPlacement

--- src/main/java/frc/robot/commands/CoralPlacement.java
+++ src/main/java/frc/robot/commands/CoralPlacement.java
@@ -1,7 +1,8 @@
 package frc.robot.commands;
 
+import java.util.Set;
+
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Crane;
@@ -23,15 +24,11 @@ public class CoralPlacement extends SequentialCommandGroup {
     m_crane = crane;
     m_fieldPoseUtil = fieldPoseUtil;
     addRequirements(m_drive, m_handler, m_crane);
-
-    Command driveToPose = new DriveToPose(
-      m_fieldPoseUtil.getTargetPoseAtReef(m_fieldPoseUtil.closestReefHour(m_drive.getPose()), subPose),
-      drive);
-
+    
     addCommands(
-      driveToPose,
-      new Strafe(handler, drive)
-        .until(() -> m_handler.isReefAligned()),
+      Commands.defer((() -> {return new DriveToPose(
+        m_fieldPoseUtil.getTargetPoseAtReef(m_fieldPoseUtil.closestReefHour(m_drive.getPose()), subPose),
+        drive);}), Set.of(drive)),
       Commands.runOnce(() -> m_crane.moveTo(cranePosition)),
       Commands.waitUntil(() -> m_crane.atGoal().isPresent()),
       Commands.runOnce(() -> m_handler.eject()),
