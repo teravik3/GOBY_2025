33e95bcb8f905f1ef8ea735d6d4bd9daf0febd37
Jason Evans <jasone@canonware.com>
2025-03-25 14:52:45 -0700

    Fix/parallelize GetCoral

--- src/main/java/frc/robot/commands/GetCoral.java
+++ src/main/java/frc/robot/commands/GetCoral.java
@@ -12,28 +12,22 @@ import frc.robot.utilities.FieldPoseUtil;
 import frc.robot.utilities.FieldPoseUtil.CoralStationSubPose;
 
 public class GetCoral extends SequentialCommandGroup {
-  private final DriveSubsystem m_drive;
-  private final HandlerSubsystem m_handler;
-  private final Crane m_crane;
-  private final FieldPoseUtil m_fieldPoseUtil;
-
   public GetCoral(DriveSubsystem drive, HandlerSubsystem handler, Crane crane,
       FieldPoseUtil fieldPoseUtil, CoralStationSubPose subPose) {
-    m_drive = drive;
-    m_handler = handler;
-    m_crane = crane;
-    m_fieldPoseUtil = fieldPoseUtil;
-    addRequirements(m_drive, m_handler, m_crane);
+    addRequirements(drive, handler, crane);
 
     addCommands(
-      Commands.defer((() -> {return new DriveToPose(
-        m_fieldPoseUtil.getTargetPoseAtStation(m_fieldPoseUtil.closestStation(m_drive.getPose()), subPose),
-        drive);}), Set.of(drive)),
-      Commands.runOnce(() -> m_crane.moveTo(CraneConstants.kPositionIntake)),
-      Commands.runOnce(() -> m_handler.intakeCoral()),
-      Commands.waitUntil(() -> m_handler.isLoadedCoral()),
-      Commands.runOnce(() -> m_crane.moveTo(CraneConstants.kPositionHome)),
-      Commands.waitUntil(() -> m_crane.atGoal().isPresent())
+      Commands.parallel(
+        Commands.defer((() -> {return new DriveToPose(
+          fieldPoseUtil.getTargetPoseAtStation(
+            fieldPoseUtil.closestStation(drive.getPose()), subPose),
+          drive);}), Set.of(drive)),
+        Commands.runOnce(() -> crane.moveTo(CraneConstants.kPositionIntake), crane)
+      ),
+      Commands.runOnce(() -> handler.intakeCoral(), handler),
+      Commands.waitUntil(() -> handler.isLoadedCoral()),
+      Commands.runOnce(() -> crane.moveTo(CraneConstants.kPositionHome), crane),
+      Commands.waitUntil(() -> crane.atGoal().isPresent())
     );
   }
 }
